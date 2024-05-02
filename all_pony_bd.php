3fd3474bb58d9841a697df035cf728cc4882f070
XDsiroriginal <144023827+XDsiroriginal@users.noreply.github.com>
2024-05-02 12:50:14 +0700

    Update all_pony_bd.php

--- all_pony_bd.php
+++ all_pony_bd.php
@@ -1,8 +1,3 @@
 <?php
-
-$connect = mysqli_connect(host: 'localhost', user: 'root', password: 'root', database: 'all_pony';
-
-if (!$connect) 
-{
-	echo 'БЛЯТЬ ВСЁ ПО ПИЗДЕ ПОШЛО';
-}
+	require_once 'connect.php';
+?>
